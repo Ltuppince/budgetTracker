993c8a13b595a9282a1bc24b601dbdab42b6ff04
Lorenzo Tuppince <ltuppince@gmail.com>
2020-03-20 23:07:24 -0400

    connect database

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,19 @@
 const FILES_TO_CACHE = [
     "/",
-    "./icons",
-    "./index.html",
-    "./index.js",
-    "./styles.css"
+    "/index.html",
+    "/styles.css",
+    "/index.js",
+    "/db.js",
+    "/manifest.webmanifest",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
 ];
 
-const STATIC_CACHE = "static-cache-v1";
-const RUNTIME_CACHE = "runtime-cache";
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// const STATIC_CACHE = "static-cache-v1";
+// const RUNTIME_CACHE = "runtime-cache";
 
 self.addEventListener("install", event => {
   event.waitUntil(
